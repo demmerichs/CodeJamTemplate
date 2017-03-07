7ff59f3213ca6ab7525f4cf5cd62739c5db7700b
David Schmidt <david.schmidt@rwth-aachen.de>
2017-03-07 12:28:08 +1000

    add bash to create competition folders

---
+++ createFolders.sh
@@ -0,0 +1,21 @@
+rm -rf A
+rm -rf B
+rm -rf C
+rm -rf D
+
+mkdir A
+cp Main.cpp A/Main.cpp
+cp sample.txt A/sample.txt
+
+mkdir B
+cp Main.cpp B/Main.cpp
+cp sample.txt B/sample.txt
+
+mkdir C
+cp Main.cpp C/Main.cpp
+cp sample.txt C/sample.txt
+
+exit
+mkdir D
+cp Main.cpp D/Main.cpp
+cp sample.txt D/sample.txt
