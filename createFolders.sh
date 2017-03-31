b82a50ae7479768e4dbf85fac5743334c14f5590
David Schmidt <david.schmidt@rwth-aachen.de>
2017-03-31 21:22:50 +1000

    bashscript arguments

--- createFolders.sh
+++ createFolders.sh
@@ -2,6 +2,7 @@ rm -rf A
 rm -rf B
 rm -rf C
 rm -rf D
+rm -rf E
 
 mkdir A
 cp Main.cpp A/Main.cpp
@@ -15,7 +16,44 @@ mkdir C
 cp Main.cpp C/Main.cpp
 cp sample.txt C/sample.txt
 
-exit
 mkdir D
 cp Main.cpp D/Main.cpp
 cp sample.txt D/sample.txt
+
+mkdir E
+cp Main.cpp E/Main.cpp
+cp sample.txt E/sample.txt
+
+file="$1"
+lastLetter="$2"
+
+if [[ $file = "" || $lastLetter = "" ]]
+then
+    echo You must provide 2 arguments:
+    echo "  First: file location"
+    echo "  Second: letter of the last problem!"
+    exit
+fi
+
+mkdir "$file"
+cp -r A "$file"
+if [ $lastLetter = "A" ]
+then
+    exit
+fi
+cp -r B "$file"
+if [ $lastLetter = "B" ]
+then
+    exit
+fi
+cp -r C "$file"
+if [ $lastLetter = "C" ]
+then
+    exit
+fi
+cp -r D "$file"
+if [ $lastLetter = "D" ]
+then
+    exit
+fi
+cp -r E "$file"
