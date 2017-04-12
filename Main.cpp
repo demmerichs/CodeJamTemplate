179e1ad0439c761b04070b4594b957eb8c7be7fe
David Schmidt <david.schmidt@rwth-aachen.de>
2017-04-12 11:55:13 +1000

    add << operator for pair

--- Main.cpp
+++ Main.cpp
@@ -22,12 +22,24 @@
 #include <algorithm>
 
 template <typename T>
+std::ostream& operator<<(std::ostream& os, std::deque<T> vector);
+
+template <typename S, typename T>
+std::ostream& operator<<(std::ostream& os, std::pair<S,T> pa);
+
+template <typename T>
 std::ostream& operator<<(std::ostream& os, std::deque<T> vector){
 	if(vector.size()==0)
 		return os;
 	os << vector[0];
 	for(unsigned long long i=1;i<vector.size();++i)
-		os << " " << vector[i];
+		os << std::endl << vector[i];
+	return os;
+}
+
+template <typename S, typename T>
+std::ostream& operator<<(std::ostream& os, std::pair<S,T> pa){
+	os << pa.first << " " << pa.second;
 	return os;
 }
 
