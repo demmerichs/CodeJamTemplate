b66163f975d7cd2c0fd897561ed334702584da60
David Schmidt <david_josef.schmidt@daimler.com>
2019-10-04 17:59:54 +0200

    add precommit hooks for diffs in examples

---
+++ hooks/check_diffs.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+shopt -s globstar
+./Code\ Jam/mainCreation.bash
+
+for d in examples/**/diff_*
+do
+    [[ $d =~ ^(.*)diff_(.*)$ ]]
+    # echo ${BASH_REMATCH[0]}
+    # echo ${BASH_REMATCH[1]}
+    # echo ${BASH_REMATCH[2]}
+    file=${BASH_REMATCH[1]}${BASH_REMATCH[2]}
+    # echo $file
+    if [ "$(diff $d <(diff Code\ Jam/Main.cpp $file))" ]
+    then
+        echo $d and $file did not match up!
+        exit 1
+    else
+        echo $d and $file did match up!
+    fi
+done
+exit 0
