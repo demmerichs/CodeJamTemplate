3a100ee480b1bb5b168e1eb1b5dcb170ce522576
David Schmidt <DavidSchmidt314@gmail.com>
2020-03-29 15:24:36 +0200

    add overview of examples with links

---
+++ hooks/create_examples_implementation_tree.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+examples_readme=examples/README.md
+echo "# Implementation of examples" > $examples_readme
+echo >> $examples_readme
+tree -P 'Main.cpp|Solution.py|diff_Main.cpp|diff_Solution.py' -vf --noreport -I '*~' --charset ascii examples \
+    | tail -n +2 \
+    | sed -e's/| /  /g' -e 's/    /  /g' -e 's/[|`]-\+/*/g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' \
+    >> $examples_readme
+
+git add $examples_readme
+exit 0
