f59eb47a177823bc9dc4a6d1e6ffe35f43b2756b
David Josef Emmerichs <david_josef.emmerichs@daimler.com>
2021-03-28 19:49:45 +0200

    distributed code jam seems to be not returning, remove it from repo

--- Distributed/head.cpp
+++
@@ -1,8 +0,0 @@
-#include "TODO.h"
-//#define DEFAULT_VAL    //remove comment on this line, to activate default value trigger
-#define ERROR_WORD "IMPOSSIBLE"
-#define COMM_TYPE ll
-
-//#region template code
-#include <message.h>
-#include <bits/stdc++.h>
--- Distributed/mainCreation.bash
+++
@@ -1,8 +0,0 @@
-#/bin/bash!
-
-IFS=$'\n'
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
-
-T=$DIR'/../tools'
-
-cat $DIR/head.cpp <(echo) $T/abbr.cpp <(echo) $T/printerTools.cpp <(echo) $T/selectionTools.cpp <(echo) $T/mathTools.cpp <(echo) $T/algoTools.cpp <(echo) $T/parallelTools.cpp <(echo) $T/messageTools.cpp <(echo) $DIR/tail.cpp > $DIR/Main.cpp
