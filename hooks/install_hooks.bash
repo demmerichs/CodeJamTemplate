b9c41c9c93fb76922d9d74c46c1b067ffc4015b5
David Josef Emmerichs <david_josef.emmerichs@daimler.com>
2021-03-28 19:38:59 +0200

    add install script for hooks

---
+++ hooks/install_hooks.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd -P)"
+
+cd $SRC_DIR
+
+cp hooks/pre-commit .git/hooks/.
+chmod +x .git/hooks/pre-commit
+
+cp hooks/create_examples_implementation_tree.bash .git/hooks/.
+chmod +x .git/hooks/create_examples_implementation_tree.bash
+
+cp hooks/create_completes_from_diff.bash .git/hooks/.
+chmod +x .git/hooks/create_completes_from_diff.bash
+
+cp hooks/create_diffs.bash .git/hooks/.
+chmod +x .git/hooks/create_diffs.bash
+
+cp hooks/check_diffs.bash .git/hooks/.
+chmod +x .git/hooks/check_diffs.bash
+
+cp hooks/check_results.bash .git/hooks/.
+chmod +x .git/hooks/check_results.bash
