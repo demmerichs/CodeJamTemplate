67500fe2a8591e78de5c2bb1ade98fbc30e96e45
David Schmidt <DavidSchmidt314@gmail.com>
2021-04-02 10:32:52 +0200

    add 2017 Round3 googlements solution

---
+++ examples/2017/Round3/A/diff_Main.cpp
@@ -0,0 +1,60 @@
+diff --git a/CodeJam/Main.cpp b/examples/2017/Round3/A/Main.cpp
+index 16602b7..1567c95 100644
+--- a/CodeJam/Main.cpp
++++ b/examples/2017/Round3/A/Main.cpp
+@@ -1013,11 +1013,55 @@ namespace task {
+ void init(){
+ }
+ 
++str G;
++ll N;
++
+ void readInput(){
++    cin >> G;
++    N = G.sz;
++}
++
++ll recur(str g){
++    llog("starting recur");
++    lg(g);
++    ll s=0;
++    forn(i, N) s+= (g[i]-'0')*(i+1);
++    if(s>N){
++        ll ans=1;
++        ll rd=N;
++        forn(i, N){
++            ans *= choosell(rd, g[i]-'0');
++            rd -=g[i]-'0';
++        }
++        llog("early break");
++        lg(ans);
++        return ans+1;
++    }
++
++    ll ans=0;
++    str curg="";
++    forn(i, N){
++        forn(j, g[i]-'0') curg += '1'+i;
++    }
++    while(curg.sz < N) curg += '0';
++    sort(all(curg));
++    lg(curg);
++    while(true){
++        if(curg!=g){
++            llog("answer before recur");
++            lg(ans);
++            ans += recur(curg);
++            llog("answer after recur");
++            lg(ans);
++        }
++        if(!next_permutation(all(curg))) break;
++    }
++    return ans + 1;
+ }
+ 
+ // write to COMM_TYPE result
+ void calcFunction() {
++    result = recur(G);
+ }
+ 
+ } // namespace task
