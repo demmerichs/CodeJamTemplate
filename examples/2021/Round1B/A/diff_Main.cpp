b43518f502c544e4fb83728fe81351e477ff8b83
David Josef Emmerichs <david_josef.emmerichs@daimler.com>
2021-04-28 18:45:01 +0200

    implement GCJ 2021 Round 1B Solution A

---
+++ examples/2021/Round1B/A/diff_Main.cpp
@@ -0,0 +1,65 @@
+diff --git a/CodeJam/Main.cpp b/examples/2021/Round1B/A/Main.cpp
+index 42da9e7..7fc0a80 100644
+--- a/CodeJam/Main.cpp
++++ b/examples/2021/Round1B/A/Main.cpp
+@@ -5,7 +5,7 @@
+ #define IA_ERROR_CODE -1
+ #define IA_COMM_LOG    //add comment on this line, to deactivate the interactive communication error log
+ // #define XY_NOTATION    //remove commment on this line, to activate xy notation on complex numbers
+-#define COMM_TYPE ll
++#define COMM_TYPE v(ll)
+ 
+ // The maintained and empty code template can be found at:
+ // https://github.com/demmerichs/CodeJamTemplate
+@@ -66,7 +66,7 @@ typedef std::stringstream sstr;
+ #define INF std::numeric_limits<long long>::max()
+ #define EPS 1e-15L
+ #define PI 3.14159265358979323846264338328L
+-#define MOD 1000000007LL
++#define MOD 43200000000000LL
+ #define DIR_NAMES "ENWS"
+ v(cell) DIRECTIONS = {cell(1, 0), cell(0, 1), cell(-1, 0), cell(0, -1)};
+ d(char, ll) DIR_IDX = {{'E', 0}, {'N', 1}, {'W', 2}, {'S', 3}};
+@@ -1024,11 +1024,42 @@ void init(){
+     cin >> T;
+ }
+ 
++ll A, B, C;
++
+ void readInput(){
++    cin >> A >> B >> C;
+ }
+ 
+ // write to COMM_TYPE result
+ void calcFunction() {
++    result.resize(4);
++    v(ll) HMS = {A, B, C};
++    sort(all(HMS));
++    do{
++        ll H=HMS[0], M=HMS[1], S=HMS[2];
++        lg(H);
++        lg(M);
++        lg(S);
++        ll mminushmodT = mposOp(M-H);
++        ll t = mdivOp(mminushmodT, 11LL);
++        ll delta = H-t;
++        if(M != mposOp(12*t+delta)){
++            llog("minutes did not match");
++            continue;
++        }
++        if(S != mposOp(720*t+delta)){
++            llog("seconds did not match");
++            continue;
++        }
++        result[3] = t % 1000000000LL;
++        t /= 1000000000LL;
++        result[2] = t % 60LL;
++        t /= 60LL;
++        result[1] = t % 60LL;
++        t /= 60LL;
++        result[0] = t;
++        break;
++    }while(next_permutation(all(HMS)));
+ }
+ 
+ } // namespace task
