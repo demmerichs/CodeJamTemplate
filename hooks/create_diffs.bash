da4be59f25b65ac14c5042c75a576214371e0968
David Josef Emmerichs <david_josef.emmerichs@daimler.com>
2021-05-29 17:00:08 +0200

    modify diff creation script in order to create diffs for single files

--- hooks/create_diffs.bash
+++ hooks/create_diffs.bash
@@ -3,10 +3,20 @@
 shopt -s globstar
 ./CodeJam/mainCreation.bash
 
-if [[ $1 != "-f" ]]
+if [[ -z $1 ]]
 then
     echo "You probably do not want to use this script anymore, because it is better to use old diffs to recreate the solution files!"
     echo "If you want to force run this, please provide as argument '-f'!"
+    exit 0
+fi
+
+if [[ $1 != "-f" ]]
+then
+    d=$1
+    [[ $d =~ ^(.*)/(Main.cpp|Solution.py.m4)$ ]]
+    output_file=${BASH_REMATCH[1]}/diff_${BASH_REMATCH[2]}
+    basefile=${BASH_REMATCH[2]}
+    git diff --no-index CodeJam/$basefile $d > $output_file
     exit 0
 fi
 
