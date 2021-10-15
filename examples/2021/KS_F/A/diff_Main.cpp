cb887353572ddddf2b21ed465bdbcff4c39d70ef
David Josef Emmerichs <david_josef.emmerichs@daimler.com>
2021-10-15 16:15:24 +0200

    add solution A for 2021 KS F

---
+++ examples/2021/KS_F/A/diff_Main.cpp
@@ -0,0 +1,46 @@
+diff --git a/CodeJam/Main.cpp b/examples/2021/KS_F/A/Main.cpp
+index 883b228..5f58bd1 100644
+--- a/CodeJam/Main.cpp
++++ b/examples/2021/KS_F/A/Main.cpp
+@@ -1065,11 +1065,41 @@ void init(){
+     cin >> T;
+ }
+ 
++ll N;
++str S;
++
+ void readInput(){
++    cin >> N;
++    cin >> S;
+ }
+ 
+ // write to COMM_TYPE result
+ void calcFunction() {
++    result = 0;
++    v(ll) walkleft, walkright;
++
++    ll cur = INF/2;
++    forn(i, N){
++        if(S[i] == '0')
++            walkleft.pb(cur+1);
++        else
++            walkleft.pb(0);
++        cur = walkleft.bk;
++    }
++    cur = INF/2;
++    forn(i, N){
++        ll j = N-1-i;
++        if(S[j] == '0')
++            walkright.pb(cur + 1);
++        else
++            walkright.pb(0);
++        cur = walkright.bk;
++    }
++
++    forn(i, N){
++        ll j = N-1-i;
++        result += min(walkleft[i], walkright[j]);
++    }
+ }
+ 
+ } // namespace task
