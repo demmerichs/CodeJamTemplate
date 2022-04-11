5bca9ae0250cbec681c6a24b9fa0f2f90cfdd452
David Josef Emmerichs <david_josef.emmerichs@daimler.com>
2022-04-11 13:05:24 +0200

    implement testcases for basic operations on bigints

---
+++ examples/template_tests/bigint/div/create_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import itertools as it
+
+ri = np.random.randint  # produces random numbers from [l, r)
+rf = np.random.rand
+perm = np.random.permutation
+
+
+def bigri(bits):
+    if bits % 32:
+        morebits = (bits // 32 + 1) * 32
+        result = bigri(morebits)
+        excessbits = morebits - bits
+        return result >> excessbits
+
+    result = 0
+    while bits > 0:
+        newbits = ri(0, 1 << 32)
+        result <<= 32
+        result += newbits
+        bits -= 32
+    assert bits == 0
+    return result
+
+
+def sign(a):
+    if a > 0:
+        return 1
+    if a < 0:
+        return -1
+    if a == 0:
+        return 0
+
+
+def cpp_div(a, b):
+    sgn = sign(a) * sign(b)
+    return sgn * (abs(a) // abs(b))
+
+
+def cpp_modulo(a, b):
+    d = cpp_div(a, b)
+    return a - b * d
+
+
+bits = [0, 1, 2, 3, 30, 31, 32, 33, 34, 35, 60, 61, 62, 63, 64, 65, 66, 67, 68, 94, 95, 96, 97, 98, 99, 125, 126, 127, 128, 129, 130, 131, 256]
+
+with open('sample.txt', 'w') as fsample:
+    with open('result.txt', 'w') as fresult:
+        fsample.write('%d\n' % (64 * len(bits)**2))
+        for tA, bA in enumerate(bits):
+            As = [1 << bA, (1 << bA) + 1, (1 << bA) - 1, bigri(bA)]
+            As += [-a for a in As]
+            for tB, bB in enumerate(bits):
+                Bs = [1 << bB, (1 << bB) + 1, (1 << bB) - 1, bigri(bB) + 1]
+                Bs += [-b for b in Bs]
+                for ti, (a, b) in enumerate(it.product(As, Bs)):
+                    t = tA * len(bits) + tB
+                    t *= 64
+                    t += ti + 1
+                    if b == 0:
+                        b = 1
+                    fsample.write('%d %d\n' % (a, b))
+                    fresult.write('Case #%d: %d\n' % (t, cpp_div(a, b)))
---
+++ examples/template_tests/bigint/mod/diff_Main.cpp
@@ -0,0 +1,29 @@
+diff --git a/CodeJam/Main.cpp b/examples/template_tests/bigint/mod/Main.cpp
+index 9a2018d..c835e47 100644
+--- a/CodeJam/Main.cpp
++++ b/examples/template_tests/bigint/mod/Main.cpp
+@@ -5,7 +5,7 @@
+ #define IA_ERROR_CODE -1
+ #define IA_COMM_LOG    //add comment on this line, to deactivate the interactive communication error log
+ // #define XY_NOTATION    //remove commment on this line, to activate xy notation on complex numbers
+-#define COMM_TYPE ll
++#define COMM_TYPE bint
+ 
+ // The maintained and empty code template can be found at:
+ // https://github.com/demmerichs/CodeJamTemplate
+@@ -1411,11 +1411,15 @@ void init(){
+     cin >> T;
+ }
+ 
++bint a, b;
++
+ void readInput(){
++    cin >> a >> b;
+ }
+ 
+ // write to COMM_TYPE result
+ void calcFunction() {
++    result = a % b;
+ }
+ 
+ } // namespace task
