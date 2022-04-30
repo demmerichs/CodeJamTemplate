6e7730d7b550c9d3194c46edc6d0474803d01a8d
David Josef Emmerichs <david_josef.emmerichs@daimler.com>
2022-04-30 14:16:19 +0200

    add solution A B for 2022 CJ Round 1C

---
+++ examples/2022/Round1C/A/diff_Main.cpp
@@ -0,0 +1,97 @@
+diff --git a/CodeJam/Main.cpp b/examples/2022/Round1C/A/Main.cpp
+index a7f0d62..1e8a7e4 100644
+--- a/CodeJam/Main.cpp
++++ b/examples/2022/Round1C/A/Main.cpp
+@@ -5,7 +5,7 @@
+ #define IA_ERROR_CODE -1
+ #define IA_COMM_LOG    //add comment on this line, to deactivate the interactive communication error log
+ // #define XY_NOTATION    //remove commment on this line, to activate xy notation on complex numbers
+-#define COMM_TYPE ll
++#define COMM_TYPE str
+ 
+ // The maintained and empty code template can be found at:
+ // https://github.com/demmerichs/CodeJamTemplate
+@@ -1501,11 +1501,83 @@ void init(){
+     cin >> T;
+ }
+ 
++ll N;
++v(str) Si;
++
+ void readInput(){
++    cin >> N;
++    Si.resize(N);
++    forn(i, N)
++        cin >> Si[i];
++}
++
++v(str) reduce_letter(char let, v(str) reductions){
++    v(str) rest, left, center, right;
++    str allsameconcat = "";
++    foreach(s, reductions){
++        if(s.find(let) == str::npos){
++            rest.pb(s);
++            continue;
++        }
++        ll l=s.sz, r=0, count=0;
++        forn(i, s.sz){
++            if(s[i]==let){
++                l = min(l, i);
++                r = max(r, i);
++                count++;
++            }
++        }
++        if(count < r - l + 1){
++            return v(str)();
++        }
++        if(l == 0 && r == s.sz-1){
++            allsameconcat += s;
++            continue;
++        }
++        if(l == 0){
++            left.pb(s);
++            continue;
++        }
++        if(r==s.sz-1){
++            right.pb(s);
++            continue;
++        }
++        center.pb(s);
++    }
++    str contigious = "";
++    if(right.sz>=2)
++        return v(str)();
++    if(right.sz == 1)
++        contigious += right[0];
++    contigious += allsameconcat;
++    if(left.sz >= 2)
++        return v(str)();
++    if(left.sz == 1)
++        contigious += left[0];
++    if(center.sz >= 2)
++        return v(str)();
++    if(center.sz == 1 && contigious.sz > 0)
++        return v(str)();
++    if(center.sz == 1)
++        contigious += center[0];
++    if(contigious.sz > 0)
++        rest.pb(contigious);
++    return rest;
+ }
+ 
+ // write to COMM_TYPE result
+ void calcFunction() {
++    v(str) reductions = Si;
++    forn(i, 26){
++        reductions = reduce_letter('A'+i, reductions);
++        if(reductions.sz == 0){
++            result = "IMPOSSIBLE";
++            return;
++        }
++    }
++    result = "";
++    foreach(s, reductions)
++        result += s;
+ }
+ 
+ } // namespace task
---
+++ examples/2022/Round1C/B/diff_Main.cpp
@@ -0,0 +1,66 @@
+diff --git a/CodeJam/Main.cpp b/examples/2022/Round1C/B/Main.cpp
+index a7f0d62..31218f8 100644
+--- a/CodeJam/Main.cpp
++++ b/examples/2022/Round1C/B/Main.cpp
+@@ -1,11 +1,11 @@
+-// #define DEFAULT_VAL_MODE    //remove comment on this line, to activate default value trigger
++#define DEFAULT_VAL_MODE    //remove comment on this line, to activate default value trigger
+ #define DEFAULT_VAL_TRIGGER result.sz == 0
+ #define DEFAULT_VAL "IMPOSSIBLE"
+ // #define IA_MODE        //remove comment on this line, to activate interactive problem mode
+ #define IA_ERROR_CODE -1
+ #define IA_COMM_LOG    //add comment on this line, to deactivate the interactive communication error log
+ // #define XY_NOTATION    //remove commment on this line, to activate xy notation on complex numbers
+-#define COMM_TYPE ll
++#define COMM_TYPE v(ll)
+ 
+ // The maintained and empty code template can be found at:
+ // https://github.com/demmerichs/CodeJamTemplate
+@@ -1501,11 +1501,47 @@ void init(){
+     cin >> T;
+ }
+ 
++ll N, K;
++v(ll) Ei;
++
+ void readInput(){
++    cin >> N >> K;
++    Ei.resize(N);
++    forn(i, N){
++        cin >> Ei[i];
++        lg(Ei);
++    }
+ }
+ 
+ // write to COMM_TYPE result
+ void calcFunction() {
++    ll A = 0, S=0;
++    forn(i, N){
++        A += Ei[i]*Ei[i];
++        S += Ei[i];
++    }
++    lg(A);
++    lg(S);
++    ll delta = (A-S*S)/2;
++    lg(delta);
++    result.cl;
++    if(delta == 0){
++        result.pb(0);
++        return;
++    }
++    if(K==1){
++        if(S == 0)
++            return;
++        if(delta % S == 0){
++            result.pb(delta/S);
++        }
++        return;
++    }
++    lg(result);
++    result.pb(1-S);
++    lg(result);
++    result.pb(delta + S*S - S);
++    lg(result);
+ }
+ 
+ } // namespace task
